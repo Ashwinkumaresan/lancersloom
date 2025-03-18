748920bd4ba28755e4d9e8f8509729250d3b22e5
Ashwinkumaresan <achusuchu123@gmail.com>
2025-03-18 19:47:12 +0530

    Initial commit 2

--- src/pages/HOme.jsx
+++ src/pages/HOme.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import Hero from '../components/home/hero'
+import Hero from '../components/home/Hero'
 import About from '../components/home/About'
 import Skills from '../components/home/Skills'
 import Portfolio from '../components/home/Portfolio'
